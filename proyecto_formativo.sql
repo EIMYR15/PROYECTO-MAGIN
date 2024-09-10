d0af3acf8d2f41efc98ca2c6026b727b54d4263d
GzFabri <faher93@gmail.com>
2024-09-10 15:26:36 -0500

    creacion de tabla cliente, region, provincia, departamento y estado_federado

--- proyecto_formativo.sql
+++ proyecto_formativo.sql
@@ -112,4 +112,75 @@ ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE proyecto_magin.Cliente (
+	id INT UNSIGNED auto_increment NOT NULL,
+	Nit_cliente INT UNSIGNED NULL,
+	Numero varchar(80) NULL,
+	Nombre varchar(150) NULL,
+	Direccion varchar(150) NULL,
+	Region_id INT UNSIGNED NULL,
+	Fijo varchar(50) NULL,
+	Celular varchar(50) NULL,
+	Correo varchar(100) NULL,
+	Fecha_registro DATETIME NULL,
+	CONSTRAINT Cliente_PK PRIMARY KEY (id)
+	ALTER TABLE proyecto_magin.Cliente ADD CONSTRAINT Cliente_Nit_cliente_FK FOREIGN KEY (Nit_cliente_id) REFERENCES proyecto_magin.Nit_cliente(id);
+	ALTER TABLE proyecto_magin.Cliente ADD CONSTRAINT Cliente_Region_FK FOREIGN KEY (Region_id) REFERENCES proyecto_magin.Region(id);
+
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE proyecto_magin.Departamento (
+	id INT UNSIGNED auto_increment NOT NULL,
+	Nombre varchar(100) NULL,
+	Pais_id INT UNSIGNED NULL,
+	CONSTRAINT Departamento_PK PRIMARY KEY (id)
+	ALTER TABLE proyecto_magin.Departamento ADD CONSTRAINT Departamento_Pais_FK FOREIGN KEY (Pais_id) REFERENCES proyecto_magin.Pais(id);
+
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE proyecto_magin.Provincia (
+	id INT UNSIGNED auto_increment NOT NULL,
+	Nombre varchar(100) NULL,
+	Pais_id INT UNSIGNED NULL,
+	CONSTRAINT Provincia_PK PRIMARY KEY (id)
+	ALTER TABLE proyecto_magin.Provincia ADD CONSTRAINT Provincia_Pais_FK FOREIGN KEY (Pais_id) REFERENCES proyecto_magin.Pais(id);
+
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE proyecto_magin.Estado_federado (
+	id INT UNSIGNED auto_increment NOT NULL,
+	Nombre varchar(100) NULL,
+	Pais_id INTEGER UNSIGNED NULL,
+	CONSTRAINT Estado_federado_PK PRIMARY KEY (id)
+	ALTER TABLE proyecto_magin.Estado_federado ADD CONSTRAINT Estado_federado_Pais_FK FOREIGN KEY (Pais_id) REFERENCES proyecto_magin.Pais(id);
+
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE proyecto_magin.Region (
+	id INT UNSIGNED auto_increment NOT NULL,
+	Pais_id INT UNSIGNED NULL,
+	Departamento_id INT UNSIGNED NULL,
+	Provincia_id INT UNSIGNED NULL,
+	Estado_federado_id INT UNSIGNED NULL,
+	CONSTRAINT Region_PK PRIMARY KEY (id),
+	CONSTRAINT Region_Pais_FK FOREIGN KEY (Pais_id) REFERENCES proyecto_magin.Pais(id),
+	CONSTRAINT Region_Departamento_FK FOREIGN KEY (Departamento_id) REFERENCES proyecto_magin.Departamento(id),
+	CONSTRAINT Region_Provincia_FK FOREIGN KEY (Provincia_id) REFERENCES proyecto_magin.Provincia(id),
+	CONSTRAINT Region_Estado_federado_FK FOREIGN KEY (Estado_federado_id) REFERENCES proyecto_magin.Estado_federado(id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
 
